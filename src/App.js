02bed2953ef1126380d050339550103f2be887bb
JuhyeunLee <juhyeungx2@gmail.com>
2024-09-19 17:36:01 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,6 @@
 import React from 'react';
-import styled, { createGlobalStyle } from 'styled-components';
+import styled from 'styled-components';
 import SecretLoveMessage from './SecretLoveMessage';
-
-const GlobalStyle = createGlobalStyle`
-  @keyframes firework {
-    0% { transform: translateY(0) scale(1); opacity: 1; }
-    100% { transform: translateY(-50px) scale(0); opacity: 0; }
-  }
-
-  .animate-firework {
-    animation: firework 1s ease-out forwards;
-  }
-
-  @keyframes bounce {
-    0%, 100% { transform: translateY(-25%); animation-timing-function: cubic-bezier(0.8, 0, 1, 1); }
-    50% { transform: translateY(0); animation-timing-function: cubic-bezier(0, 0, 0.2, 1); }
-  }
-
-  .animate-bounce {
-    animation: bounce 1s infinite;
-  }
-`;
 
 const AppContainer = styled.div`
   display: flex;
@@ -32,12 +12,9 @@ const AppContainer = styled.div`
 
 function App() {
   return (
-    <>
-      <GlobalStyle />
-      <AppContainer className="App">
-        <SecretLoveMessage />
-      </AppContainer>
-    </>
+    <AppContainer className="App">
+      <SecretLoveMessage />
+    </AppContainer>
   );
 }
 
