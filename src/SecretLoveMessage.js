c52fc671cdf832dcf4c82018f30a7f9e5575e347
JuhyeunLee <juhyeungx2@gmail.com>
2024-09-19 18:08:45 +0900

    Update SecretLoveMessage.js

--- src/SecretLoveMessage.js
+++ src/SecretLoveMessage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import styled, { keyframes, css } from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { Heart, Lock } from 'lucide-react';
 
 const fadeIn = keyframes`
